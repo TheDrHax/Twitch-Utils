07d4ba683483e8b97304c660fe3250e6a6a958b2
Дмитрий Карих <the.dr.hax@gmail.com>
2020-02-10 14:45:42 +0300

    Release 1.3
    * FLV output support;
    * Force overwrite output file with `-y` or `--force`;
    * MP4 faststart disabled by default (enable with `--faststart`);
    * Attempt to fix incorrect FPS of the output when using MP4 container;

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md', 'r') as fi:
 
 setup(
     name='tdh-twitch-utils',
-    version='1.2',
+    version='1.3',
 
     author='Dmitry Karikh',
     author_email='the.dr.hax@gmail.com',
