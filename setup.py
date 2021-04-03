364d4d21476971b21811ed572a07ac2d408471c6
Дмитрий Карих <the.dr.hax@gmail.com>
2021-04-03 21:32:25 +0300

    Release 1.5

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ EXTRAS['all'] = list(itertools.chain.from_iterable(EXTRAS.values()))
 
 setup(
     name='tdh-twitch-utils',
-    version='1.4',
+    version='1.5',
 
     author='Dmitry Karikh',
     author_email='the.dr.hax@gmail.com',
